773db8c9bd4eeefc6aa191979d38c98e5e64d37d
Max <55975910+MaxZ0@users.noreply.github.com>
2021-03-14 20:12:54 +0100

    eh

--- src/components/login/LoginPage.js
+++ src/components/login/LoginPage.js
@@ -1,11 +1,12 @@
 import Heading from "../layout/Heading";
+import LoginForm from "./LoginForm";
 import { BASE_URL } from "../../constants/api";
 
 export default function LoginPage() {
-	return (
-		<>
-			<Heading content="Login" />
-			{BASE_URL}
-		</>
-	);
+  return (
+    <>
+      <Heading content="Login" />
+      <LoginForm />
+    </>
+  );
 }
